ce4a0be7cf78d01266709273a8023d7cc01e67f6
AnatolyGeorgievski <59827186+AnatolyGeorgievski@users.noreply.github.com>
2024-10-23 00:47:27 +0300

    Add files via upload

--- src/cpuid.c
+++ src/cpuid.c
@@ -1,3 +1,9 @@
+/*! 
+	\author Анатолий Георгиевский 
+сборка:
+	$gcc -DTEST_CPUID -o cpuid cpuid.c 
+*/
+
 #include <cpuid.h>
 #include <stdio.h>
 
@@ -53,6 +59,8 @@ static void  __attribute__((constructor)) cpuid_init()
 
 
     __cpuid(1, ax,bx,cx,dx);
+	// printf("Cache line: %d B\n", ((bx>>8)&0xFF)*8);
+	// printf("Max Proc IDs: %d B\n", ((bx>>16)&0xFF)*8);
 	// cache line size in bytes
     if (dx& bit_MMX)    printf(" MMX");
     if (dx& bit_SSE)    printf(" SSE");
@@ -88,6 +96,7 @@ int cpu_family_id = (ax>>8)&0xF;
 int cpu_model_id  = (ax>>4)&0xF;
 	cpu_model_id |= (cpu_family_id==6 || cpu_family_id==15)? (ax>>16<<4)&0xF0: 0;
 	cpu_family_id+= (cpu_family_id==15)? ((ax>>20)&0xFF):0;
+int avx10 = 0;
     if (cpuid_max_func>=0x7) {
         __cpuid_count(7,0, ax,bx,cx,dx);
     /*  CPUID.(EAX=07H, ECX=0H):EBX.AVX2[bit 5]==1
@@ -102,16 +111,19 @@ int cpu_model_id  = (ax>>4)&0xF;
         if (bx& (1<<11))   printf(" RTM");
         if (bx& (1<<14))   printf(" MPX");
         if (bx& (1<<16))   printf(" AVX512F");
-        if (bx& (1<<16))   printf(" AVX512DQ");
+        if (bx& (1<<17))   printf(" AVX512DQ");
         if (bx& (1<<18))   printf(" RDSEED");
         if (bx& (1<<19))   printf(" ADX");
+        if (bx& (1<<20))   printf(" SMAP");
         if (bx& (1<<21))   printf(" AVX512IFMA");
+        if (bx& (1<<23))   printf(" CLFLUSHOPT");
         if (bx& (1<<26))   printf(" AVX512PF");
         if (bx& (1<<27))   printf(" AVX512ER");
         if (bx& (1<<28))   printf(" AVX512CD");
         if (bx& (1<<29))   printf(" SHA");
         if (bx& (1<<30))   printf(" AVX512BW");
         if (bx& (1<<31))   printf(" AVX512VL");
+		
         if (cx& (1<< 1))   printf(" AVX512_VBMI"); // bit manipulation
         if (cx& (1<< 6))   printf(" AVX512_VBMI2"); // bit manipulation
         if (cx& (1<< 8))   printf(" GFNI"); // Galois Field 2^8
@@ -126,8 +138,29 @@ int cpu_model_id  = (ax>>4)&0xF;
         if (dx& (1<<2))    printf(" AVX512_4VNNIW");
         if (dx& (1<<3))    printf(" AVX512_4FMAPS");
         if (dx& (1<<4))    printf(" FSRM");
+        if (dx& (1<<14))   printf(" SERIALIZE");
+        if (dx& (1<<15))   printf(" Hybrid");
+        if (dx& (1<<16))   printf(" TSXLDTRK");
+		if (dx& (1<<22))   printf(" AMX-BF16");
+		if (dx& (1<<23))   printf(" AVX512-FP16");
+		if (dx& (1<<24))   printf(" AMX-TILE");
+		if (dx& (1<<25))   printf(" AMX-INT8");
 		__cpuid_count(7,1, ax,bx,cx,dx);
-		if (ax& (1<<3))    printf(" AVX512_BF16");
+		if (ax& (1<<0))    printf(" SHA512");
+		if (ax& (1<<1))    printf(" SM3");
+		if (ax& (1<<2))    printf(" SM4");
+		if (ax& (1<<3))    printf(" RAO-INT");
+		if (ax& (1<<4))    printf(" AVX-VNNI");
+		if (ax& (1<<5))    printf(" AVX512_BF16");
+		if (ax& (1<<21))   printf(" AMX-FP16");
+		if (ax& (1<<23))   printf(" AVX-IFMA");
+		if (dx& (1<<4))    printf(" AVX-VNNI-INT8");
+		if (dx& (1<<4))    printf(" AMX-COMPLEX");
+		if (dx& (1<<10))   printf(" AVX-VNNI-INT16");
+		if (dx& (1<<19))   {
+			printf(" AVX10");
+			avx10 = 1;
+		}
     }
     /* CPUID.(EAX=80000001H):ECX.LZCNT[bit 5]==1 */
     __cpuid(0x80000001, ax,bx,cx,dx);
@@ -141,6 +174,12 @@ int cpu_model_id  = (ax>>4)&0xF;
 
 
     printf("\n");
+	if (avx10) {
+		__cpuid_count(0x24,0, ax,bx,cx,dx);
+		printf(" AVX10 Version=%d\n", bx&0xFF);
+		//__cpuid_count(0x24,1, ax,bx,cx,dx);
+	}
+	
     if (cpuid_max_func >= 0x6)
     {// EAX=80000006h: Extended L2 Cache Features
 		__cpuid(0x80000006, ax,bx,cx,dx);
@@ -166,41 +205,88 @@ int cpu_model_id  = (ax>>4)&0xF;
     }
 #ifdef TEST_CPUID
 	char *cpu_model_name="";
-	if(cpu_family_id==6)
-	switch (cpu_model_id){
-	case 0x2A: 	cpu_model_name="Sandy Bridge"; break;
-	case 0x2D: 	cpu_model_name="Sandy Bridge-E (Server)"; break;
-	case 0x36: 	cpu_model_name="Cedar Trail 32nm"; break;
-	case 0x37: 	cpu_model_name="Bay Trail"; break;
-	case 0x3A: 	cpu_model_name="Ivy Bridge"; break;
-	case 0x3C: 	cpu_model_name="Haswell"; break;
-	case 0x3E: 	cpu_model_name="Ivy Bridge-E (Server)"; break;
-	case 0x3F: 	cpu_model_name="Haswell-E (Server)"; break;
-	case 0x4E: 	
-	case 0x55: 	cpu_model_name="Skylake (Server)"; break;
-	case 0x45:	
-	case 0x46:	cpu_model_name="Haswell"; break;
-	case 0x47:	cpu_model_name="Broadwell"; break;
-	case 0x4C:	cpu_model_name="Airmont"; break;
-	case 0x4F:
-	case 0x56: 	cpu_model_name="Broadwell (Server)"; break;
-	case 0x57: 	cpu_model_name="Knights Landing"; break;
-	case 0x5C: 	cpu_model_name="Apollo Lake"; break;
-	case 0x5D: 	cpu_model_name="Silvermont"; break;
-	case 0x5E: 	cpu_model_name="Skylake"; break;
-	case 0x5F: 	cpu_model_name="Goldmont"; break;
-	case 0x66: 	cpu_model_name="Cannon Lake"; break;
-	case 0x6A: 	
-	case 0x6C: 	cpu_model_name="Ice Lake (Server)"; break;
-	case 0x7A: 	cpu_model_name="Gemini Lake"; break;
-	case 0x7E:  cpu_model_name="Ice Lake-U/Y"; break;
-	case 0x7D: 	cpu_model_name="Ice Lake"; break;
-	case 0x85: 	cpu_model_name="Knights Mill"; break;
-	case 0x86: 	cpu_model_name="Tremont"; break;
-	case 0x8E: 	
-	case 0x9E: 	cpu_model_name="Kaby Lake"; break;
-	default: 	cpu_model_name=""; break;
+	if(cpu_family_id==6){
+		switch (cpu_model_id){
+		case 0x2A: 	cpu_model_name="Sandy Bridge"; break;
+		case 0x2D: 	cpu_model_name="Sandy Bridge-E (Server)"; break;
+		case 0x36: 	cpu_model_name="Cedar Trail 32nm"; break;
+		case 0x37: 	cpu_model_name="Bay Trail"; break;
+		case 0x3A: 	cpu_model_name="Ivy Bridge"; break;
+		case 0x3C: 	cpu_model_name="Haswell-S"; break;
+		case 0x3E: 	cpu_model_name="Ivy Bridge-E (Server)"; break;
+		case 0x3F: 	cpu_model_name="Haswell-E (Server)"; break;
+		case 0x4E: 	cpu_model_name="Skylake-U/Y"; break; 	
+		case 0x55: 	cpu_model_name="Skylake (Server)"; break;
+		case 0x45:	
+		case 0x46:	cpu_model_name="Haswell"; break;
+		case 0x47:	cpu_model_name="Broadwell"; break;
+		case 0x4C:	cpu_model_name="Airmont"; break;
+		case 0x4F:
+		case 0x56: 	cpu_model_name="Broadwell (Server)"; break;
+		case 0x57: 	cpu_model_name="Knights Landing"; break;
+		case 0x5C: 	cpu_model_name="Apollo Lake"; break;
+		case 0x5D: 	cpu_model_name="Silvermont"; break;
+		case 0x5E: 	cpu_model_name="Skylake"; break;
+		case 0x5F: 	cpu_model_name="Goldmont"; break;
+		case 0x66: 	cpu_model_name="Cannon Lake"; break;
+		case 0x6A: 	
+		case 0x6C: 	cpu_model_name="Ice Lake (Server)"; break;
+		case 0x7A: 	cpu_model_name="Gemini Lake"; break;
+		case 0x7E:  cpu_model_name="Ice Lake-U/Y"; break;
+		case 0x7D: 	cpu_model_name="Ice Lake"; break;
+		case 0x85: 	cpu_model_name="Knights Mill"; break;
+		case 0x86: 	cpu_model_name="Tremont"; break;
+		case 0x8A: 	cpu_model_name="Tremont Lakefield"; break;
+		case 0x8C: 	cpu_model_name="Tiger Lake-U"; break;
+		case 0x8D: 	cpu_model_name="Tiger Lake-H"; break;
+		case 0x8E: 	cpu_model_name="Kaby Lake"; break;
+		case 0x8F: 	cpu_model_name="Sapphire Rapids Server"; break;
+		
+		case 0x96: 	cpu_model_name="Elkhart Lake"; break;
+		case 0x97: 	cpu_model_name="Alder Lake-S"; break;
+		case 0x9A: 	cpu_model_name="Alder Lake"; break;
+		case 0x9C: 	cpu_model_name="Jasper Lake"; break;
+		case 0x9E: 	cpu_model_name="Kaby Lake"; break;
+		case 0xA5: 	cpu_model_name="Comet Lake"; break;
+		case 0xA7: 	cpu_model_name="Rocket Lake"; break;
+		case 0xAA: 	cpu_model_name="Meteor Lake"; break;
+		case 0xAD: 	cpu_model_name="Granite Rapids"; break;
+		case 0xAE: 	cpu_model_name="Granite Rapids"; break;
+		case 0xAF: 	cpu_model_name="Sierra Forest"; break;
+		case 0xB6: 	cpu_model_name="Grand Ridge"; break;
+		case 0xB7: 	cpu_model_name="Raptor Lake-S"; break;
+		case 0xBA: 	cpu_model_name="Raptor Lake"; break;
+		case 0xBD: 	cpu_model_name="Lunar Lake"; break;
+		case 0xBF: 	cpu_model_name="Raptor Lake"; break;
+		case 0xC5: 	cpu_model_name="Arrow Lake"; break;
+		case 0xC6: 	cpu_model_name="Arrow Lake"; break;
+		case 0xCC: 	cpu_model_name="Panther Lake"; break;
+		case 0xCF: 	cpu_model_name="Emerald Rapids Server"; break;
+		case 0xDD: 	cpu_model_name="Clearwater Forest"; break;
+		
+		default: 	cpu_model_name=""; break;
+		}
+	} else 
+	if (cpu_family_id==0x17){
+		switch (cpu_model_id){
+		case 0x31: 	cpu_model_name="Rome"; break;
+		default:	cpu_model_name="Zen 1/2"; break;
+		}
+	} else 
+	if (cpu_family_id==0x19){
+		switch (cpu_model_id){
+		case 0x00: 	cpu_model_name="Genesis Zen3"; break;
+		case 0x01: 	cpu_model_name="Milan Zen3"; break;// EPYC 7003
+		case 0x21: 	cpu_model_name="Veerner Zen3"; break;
+		case 0x31: 	cpu_model_name="Genoa Zen3"; break;// EPYC 9004
+		case 0x61: 	cpu_model_name="Raphael Zen4"; break;
+		default:	cpu_model_name="Zen 3/4"; break;
+		}
 	}
+	if (cpu_family_id==0x1A){
+		cpu_model_name="Zen 5";
+	}
+		
 	printf("cpuid = %02X_%02X %s\n", cpu_family_id, cpu_model_id, cpu_model_name);
 #endif
 }
